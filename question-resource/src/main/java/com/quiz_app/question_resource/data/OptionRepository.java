9f7849a2072694fa8308a9f4e75bbf242cbf8d75
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-10-28 21:53:58 +0530

    add the options repository

---
+++ question-resource/src/main/java/com/quiz_app/question_resource/data/OptionRepository.java
@@ -0,0 +1,6 @@
+package com.quiz_app.question_resource.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Options,Long> {
+}
