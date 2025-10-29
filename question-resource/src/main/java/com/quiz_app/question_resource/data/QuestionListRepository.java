1871edc254d7f685fbadf357d979d0e5ed5f5781
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-10-29 19:52:36 +0530

    add the repository for question list

---
+++ question-resource/src/main/java/com/quiz_app/question_resource/data/QuestionListRepository.java
@@ -0,0 +1,6 @@
+package com.quiz_app.question_resource.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionListRepository extends JpaRepository<QuestionList,Long> {
+}
