6c16219f852c10aee9504ed035810fe18ff59597
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-10-29 22:47:36 +0530

    remove the redundent repository file for options

--- question-resource/src/main/java/com/quiz_app/question_resource/data/OptionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.quiz_app.question_resource.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OptionRepository extends JpaRepository<Option,Long> {
-}
