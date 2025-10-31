1503d747129261b3332a25bae6ccc0eba05cfcaa
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-10-31 19:23:55 +0530

    write the exception handling logic for bad requests

--- quiz-resource/src/main/java/com/quiz_app/quiz_resource/exception/GlobalExceptionHandler.java
+++ quiz-resource/src/main/java/com/quiz_app/quiz_resource/exception/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ContentNotFound.class)
-    public ResponseEntity<ApiError> handleQuestionNotFound(ContentNotFound ex, WebRequest req){
+    public ResponseEntity<ApiError> handleContentNotFound(ContentNotFound ex, WebRequest req){
         ApiError error = new ApiError(
                 HttpStatus.NOT_FOUND.value(),
                 HttpStatus.NOT_FOUND.getReasonPhrase(),
@@ -18,5 +18,17 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequest.class)
+    public ResponseEntity<ApiError> handleBadRequest(BadRequest ex, WebRequest req){
+        ApiError error = new ApiError(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                ex.getMessage(),
+                req.getDescription(false).replace("uri=","")
+        );
+
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
     }
 }
