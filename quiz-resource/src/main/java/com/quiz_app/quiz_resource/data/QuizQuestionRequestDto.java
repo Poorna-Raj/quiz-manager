65846758806d023622b4e2d0eaf813db6a51252d
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-10-31 17:40:12 +0530

    add the request dto for quiz question class

---
+++ quiz-resource/src/main/java/com/quiz_app/quiz_resource/data/QuizQuestionRequestDto.java
@@ -0,0 +1,13 @@
+package com.quiz_app.quiz_resource.data;
+
+public class QuizQuestionRequestDto {
+    private long questionId;
+
+    public long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(long questionId) {
+        this.questionId = questionId;
+    }
+}
