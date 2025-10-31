8fe1f8df6abaa95432bbb7fd6df9926a713c6241
AroshaBandara09 <flim21038@gmail.com>
2025-10-31 18:32:32 +0530

    Implement the model class

---
+++ user-resource/src/main/java/Data/User.java
@@ -0,0 +1,52 @@
+package Data;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_user")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id" )
+    private long id;
+    @Column(name = "username")
+    private  String username;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "password")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
