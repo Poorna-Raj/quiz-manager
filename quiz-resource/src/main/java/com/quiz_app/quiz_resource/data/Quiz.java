0418393a64b04de1912d73d0f60a6415331b16ca
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-10-31 12:37:17 +0530

    add the model class for quiz

---
+++ quiz-resource/src/main/java/com/quiz_app/quiz_resource/data/Quiz.java
@@ -0,0 +1,87 @@
+package com.quiz_app.quiz_resource.data;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_quiz")
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "quiz_id")
+    private long id;
+    @Column(name = "question_list_id")
+    private long questionListId;
+    @Column(name = "question_count")
+    private int questionCount;
+    @Column(name = "created_by")
+    private String createdBy;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true)
+    @JoinColumn(name = "quiz_id")
+    private List<QuizQuestion> questions = new ArrayList<>();
+
+    @Column(name = "status")
+    private String status = "ACTIVE";
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getQuestionListId() {
+        return questionListId;
+    }
+
+    public void setQuestionListId(long questionListId) {
+        this.questionListId = questionListId;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<QuizQuestion> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<QuizQuestion> questions) {
+        this.questions = questions;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getQuestionCount() {
+        return questionCount;
+    }
+
+    public void setQuestionCount(int questionCount) {
+        this.questionCount = questionCount;
+    }
+}
