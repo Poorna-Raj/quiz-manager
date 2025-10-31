f1e66ec0d987fb95628f9a473603fe964a93901b
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-10-31 17:39:02 +0530

    add the data transfer model for question list

---
+++ quiz-resource/src/main/java/com/quiz_app/quiz_resource/data/QuestionListDto.java
@@ -0,0 +1,42 @@
+package com.quiz_app.quiz_resource.data;
+
+import java.util.List;
+
+public class QuestionListDto {
+    private long id;
+    private String topic;
+    private String name;
+    private List<Long> questions_id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Long> getQuestions_id() {
+        return questions_id;
+    }
+
+    public void setQuestions_id(List<Long> questions_id) {
+        this.questions_id = questions_id;
+    }
+}
