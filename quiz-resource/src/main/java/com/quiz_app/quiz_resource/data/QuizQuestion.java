5fc88208620b51a483fd82f96422e8b9f8099356
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-10-31 12:37:33 +0530

    add the model class for question in the quiz

---
+++ quiz-resource/src/main/java/com/quiz_app/quiz_resource/data/QuizQuestion.java
@@ -0,0 +1,40 @@
+package com.quiz_app.quiz_resource.data;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_quiz_question")
+public class QuizQuestion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "quiz_question_id")
+    private long id;
+    @Column(name = "question_id")
+    private long questionId;
+    @Column(name = "question")
+    private String questionText;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(long questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+}
