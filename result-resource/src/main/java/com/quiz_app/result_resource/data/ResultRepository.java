bd87779be35a12943d5d97379bc5006e58b4660d
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-04 16:00:44 +0530

    Create ResultRepository.java
    Add jpa class for result resource

---
+++ result-resource/src/main/java/com/quiz_app/result_resource/data/ResultRepository.java
@@ -0,0 +1,6 @@
+package com.quiz_app.result_resource.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultRepository extends JpaRepository<Result,Long> {
+}
