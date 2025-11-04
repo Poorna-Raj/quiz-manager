0484b52aa6f1cd079a80e96705acdf6b696882ca
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-04 23:11:45 +0530

    create the result question request dto class
    implement the data transfer object for result question requests

---
+++ result-resource/src/main/java/com/quiz_app/result_resource/data/ResultQuestionRequestDto.java
@@ -0,0 +1,22 @@
+package com.quiz_app.result_resource.data;
+
+public class ResultQuestionRequestDto {
+    private long questionId;
+    private String answer;
+
+    public long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(long questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
