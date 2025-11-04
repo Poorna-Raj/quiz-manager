c9d68082841d258c546566a6125e39840ec3b0f6
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-04 23:12:31 +0530

    create result question response dto
    implement the data transfer object for result question responses

---
+++ result-resource/src/main/java/com/quiz_app/result_resource/data/ResultQuestionResponseDto.java
@@ -0,0 +1,42 @@
+package com.quiz_app.result_resource.data;
+
+import jakarta.persistence.Column;
+
+public class ResultQuestionResponseDto {
+    private long id;
+    private long questionId;
+    private String answer;
+    private String correctAnswer;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(long questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
