a2b8adfe0f68e0080488b94a612fdb12927577e1
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-20 22:12:02 +0530

    implement the service class for question lists

---
+++ quiz-service/src/main/java/com/quiz_app/quiz_service/service/QuestionListService.java
@@ -0,0 +1,107 @@
+package com.quiz_app.quiz_service.service;
+
+import com.quiz_app.quiz_service.data.dto.QuestionListRequestDto;
+import com.quiz_app.quiz_service.data.dto.QuestionListResponseDto;
+import com.quiz_app.quiz_service.data.model.Question;
+import com.quiz_app.quiz_service.exception.BadRequestException;
+import com.quiz_app.quiz_service.exception.ContentNotFound;
+import com.quiz_app.quiz_service.data.model.QuestionList;
+import com.quiz_app.quiz_service.data.repository.QuestionListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuestionListService {
+    @Autowired
+    private QuestionListRepository repository;
+
+    public QuestionList createQuestionList(QuestionList list){
+        if(list.getName().isEmpty()||list.getTopic().isEmpty()){
+            throw new BadRequestException("Invalid List. Please fill all the fields!");
+        }
+
+        return repository.save(list);
+    }
+
+    public QuestionList updateQuestionList(long id,QuestionList newList){
+        Optional<QuestionList> existingList = repository.findById(id);
+
+        if(existingList.isEmpty()){
+            throw new ContentNotFound("Invalid question list for given ID");
+        }
+
+        if(newList.getName().isEmpty()||newList.getTopic().isEmpty()){
+            throw new BadRequestException("Invalid List. Please fill all the fields!");
+        }
+
+        existingList.get().setName(newList.getName());
+        existingList.get().setTopic(newList.getTopic());
+
+        if(newList.getQuestions() == null){
+            existingList.get().setQuestions(new ArrayList<>());
+        } else{
+            existingList.get().setQuestions(newList.getQuestions());
+        }
+
+        return repository.save(existingList.get());
+    }
+
+    public List<QuestionList> getAllQuestions(){
+        return repository.findAll();
+    }
+
+    public boolean deleteById(Long id){
+        if(!repository.existsById(id)){
+            throw new ContentNotFound("Invalid question list for given ID!");
+        }
+
+        repository.deleteById(id);
+        return true;
+    }
+
+    @Transactional
+    public QuestionList clearAllQuestionsById(long id){
+        QuestionList list = repository.findById(id)
+                .orElseThrow(() -> new BadRequestException("Invalid question list for given ID"));
+        list.getQuestions().clear();
+        return repository.save(list);
+    }
+
+    public QuestionList getQuestionListById(long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new BadRequestException("Invalid question list for given ID"));
+    }
+
+
+    public QuestionList mapToModel(QuestionListRequestDto list) {
+        QuestionList newList = new QuestionList();
+        newList.setName(list.getName());
+        newList.setTopic(list.getTopic());
+
+        return newList;
+    }
+
+    public QuestionListResponseDto mapToDto(QuestionList newList) {
+        QuestionListResponseDto responseDto = new QuestionListResponseDto();
+        responseDto.setId(newList.getId());
+        responseDto.setName(newList.getName());
+        responseDto.setTopic(newList.getTopic());
+
+        if (responseDto.getQuestions_id() == null) {
+            responseDto.setQuestions_id(new ArrayList<>());
+        }
+
+        if(newList.getQuestions() != null){
+            for(Question question: newList.getQuestions()){
+                responseDto.getQuestions_id().add(question.getId());
+            }
+        }
+
+        return responseDto;
+    }
+}
