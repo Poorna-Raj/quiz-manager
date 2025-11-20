fd597cecaa2ad799211668330f9797965c9d252d
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-20 22:11:36 +0530

    implement the controller class for questions

---
+++ quiz-service/src/main/java/com/quiz_app/quiz_service/controller/QuestionController.java
@@ -0,0 +1,49 @@
+package com.quiz_app.quiz_service.controller;
+
+import com.quiz_app.quiz_service.data.model.Question;
+import com.quiz_app.quiz_service.data.dto.QuestionRequestDto;
+import com.quiz_app.quiz_service.data.dto.QuestionResponseDto;
+import com.quiz_app.quiz_service.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class QuestionController {
+    @Autowired
+    private QuestionService service;
+
+    @PostMapping(path = "/questions")
+    public ResponseEntity<QuestionResponseDto> createQuestion(@RequestBody QuestionRequestDto dto){
+        Question question = service.mapToQuestion(dto);
+        Question savedQuestion = service.createQuestion(question);
+        return new ResponseEntity<>(service.mapToDto(savedQuestion), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/questions/{id}")
+    public ResponseEntity<QuestionResponseDto> getQuestionById(@PathVariable long id){
+        Question question = service.getQuestionById(id);
+        return new ResponseEntity<>(service.mapToDto(question),HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/questions/{id}")
+    public ResponseEntity<QuestionResponseDto> updateQuestionById(@PathVariable long id,@RequestBody QuestionRequestDto dto){
+        Question question = service.updateQuestionById(id, service.mapToQuestion(dto));
+        return new ResponseEntity<>(service.mapToDto(question),HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/questions/{id}")
+    public ResponseEntity<HttpStatus> deleteQuestionById(@PathVariable long id){
+        if(service.deleteQuestionById(id)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else{
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(path = "/questions/{id}/correct-answer")
+    public ResponseEntity<String> getCorrectAnswerByQuestionId(@PathVariable long id) {
+        return new ResponseEntity<>(service.getCorrectAnswerById(id),HttpStatus.OK);
+    }
+}
