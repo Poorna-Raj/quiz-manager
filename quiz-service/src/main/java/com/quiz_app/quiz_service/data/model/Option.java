076d7d4e1fd72a9fd28f5ac46220e73f3e3e1c0f
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-20 21:59:13 +0530

    implement the base class for options

---
+++ quiz-service/src/main/java/com/quiz_app/quiz_service/data/model/Option.java
@@ -0,0 +1,43 @@
+package com.quiz_app.quiz_service.data.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_question_options")
+public class Option {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "option_id")
+    private long id;
+
+    @Column(name = "option_text")
+    private String option_text;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOption_text() {
+        return option_text;
+    }
+
+    public void setOption_text(String option_text) {
+        this.option_text = option_text;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
