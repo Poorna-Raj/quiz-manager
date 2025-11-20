b1ec2093739c7b7fc6346d8929a1cd0ae4fe0664
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-20 21:59:30 +0530

    implement the base class for question lists

---
+++ quiz-service/src/main/java/com/quiz_app/quiz_service/data/model/QuestionList.java
@@ -0,0 +1,52 @@
+package com.quiz_app.quiz_service.data.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_question_list")
+public class QuestionList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_list_id")
+    private long id;
+    @Column(name = "topic")
+    private String topic;
+    @Column(name = "name", unique = true)
+    private String name;
+    @OneToMany(mappedBy = "list", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> questions;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+}
