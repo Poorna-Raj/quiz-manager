1a288c8b09b6f65265415683178886989856bdfc
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-11-20 22:01:20 +0530

    implement the data transfer object for question responses

---
+++ quiz-service/src/main/java/com/quiz_app/quiz_service/data/dto/QuestionResponseDto.java
@@ -0,0 +1,51 @@
+package com.quiz_app.quiz_service.data.dto;
+
+import java.util.List;
+
+public class QuestionResponseDto {
+    private long id;
+    private String question;
+    private List<String> options;
+    private int marks;
+    private long listId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public long getListId() {
+        return listId;
+    }
+
+    public void setListId(long listId) {
+        this.listId = listId;
+    }
+}
